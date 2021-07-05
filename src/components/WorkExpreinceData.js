b8786180975f053fb1807635744a525a71d67eb0
Ahmet Buğra Yiğiter <54077855+yigiterdev@users.noreply.github.com>
2021-07-05 16:40:44 +0300

    update: WorkExpreinceData.js

--- src/components/WorkExpreinceData.js
+++ src/components/WorkExpreinceData.js
@@ -1,15 +1,15 @@
 import React from "react";
 export const WorkExpreinceData = [
   {
-    title: "JamLab",
-    role: "Web Development Intern",
+    title: "Hipo",
+    role: "Frontend Intern",
     image:
-      "https://media-exp1.licdn.com/dms/image/C4D0BAQFeCWmZw3ujtg/company-logo_200_200/0/1609868067915?e=1622073600&v=beta&t=0-9CLblO4-xGoZ5Akud-bZ8gpxRZDG9uyBMHSQca9aE",
+      "https://media-exp1.licdn.com/dms/image/C4D0BAQEGNM1EMSvrvQ/company-logo_200_200/0/1535032959300?e=1633564800&v=beta&t=9a-GyGPAU9liL8f7-TQbQb_mZKvPOUf6Tlqj1FhIPoU",
     about_role:
-      "I'm working remotely from Turkey. I'm doing web development related tasks and work HTML, CSS, JavaScript. You can see all my tasks in my GitHub repository.",
+      "I'm working remotely. I'm Frontend intern at Hipo. I work on real-time projects.",
     cName: "card p-4",
-    begin_date: "December 2020",
-    end_date: "March 2021",
+    begin_date: "July 2021",
+    end_date: "Current",
   },
   {
     title: "eeBrain Team",
@@ -22,4 +22,17 @@ export const WorkExpreinceData = [
     begin_date: "February 2021",
     end_date: "Current",
   },
+  {
+    title: "JamLab",
+    role: "Web Development Intern",
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4D0BAQFeCWmZw3ujtg/company-logo_200_200/0/1609868067915?e=1622073600&v=beta&t=0-9CLblO4-xGoZ5Akud-bZ8gpxRZDG9uyBMHSQca9aE",
+    about_role:
+      "I'm working remotely from Turkey. I'm doing web development related tasks and work HTML, CSS, JavaScript. You can see all my tasks in my GitHub repository.",
+    cName: "card p-4",
+    begin_date: "December 2020",
+    end_date: "March 2021",
+  },
+  
+  
 ];
