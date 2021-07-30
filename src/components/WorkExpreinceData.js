2216fabea680c44e97be40ef2ae4cd5a8fc2e302
Ahmet Buğra Yiğiter <54077855+yigiterdev@users.noreply.github.com>
2021-07-30 17:38:24 +0300

    fix: Change WorkExpreinceData

--- src/components/WorkExpreinceData.js
+++ src/components/WorkExpreinceData.js
@@ -1,38 +1,36 @@
-import React from "react";
+import React from 'react';
 export const WorkExpreinceData = [
   {
-    title: "Hipo",
-    role: "Frontend Intern",
+    title: 'Hipo',
+    role: 'Frontend Intern',
     image:
-      "https://media-exp1.licdn.com/dms/image/C4D0BAQEGNM1EMSvrvQ/company-logo_200_200/0/1535032959300?e=1633564800&v=beta&t=9a-GyGPAU9liL8f7-TQbQb_mZKvPOUf6Tlqj1FhIPoU",
+      'https://media-exp1.licdn.com/dms/image/C4D0BAQEGNM1EMSvrvQ/company-logo_200_200/0/1535032959300?e=1633564800&v=beta&t=9a-GyGPAU9liL8f7-TQbQb_mZKvPOUf6Tlqj1FhIPoU',
     about_role:
       "I'm working remotely. I'm Frontend intern at Hipo. I work on real-time projects.",
-    cName: "card p-4",
-    begin_date: "July 2021",
-    end_date: "Current",
+    cName: 'card p-4',
+    begin_date: 'July 2021',
+    end_date: 'Current',
   },
   {
-    title: "eeBrain Team",
-    role: "Frontend Developer",
+    title: 'eeBrain Team',
+    role: 'Frontend Developer',
     image:
-      "https://media-exp1.licdn.com/dms/image/C4D0BAQEKh2YluuU33g/company-logo_200_200/0/1596561707122?e=1622678400&v=beta&t=EidJFLCKF8tBDVEVdli_Bf-Oh5dXeZxBraSdz5kioV8",
+      'https://media-exp1.licdn.com/dms/image/C4D0BAQEKh2YluuU33g/company-logo_200_200/0/1596561707122?e=1635984000&v=beta&t=DIOThrZktP70ugKoQv1k4gZ9AmEajc0yTU6r2PgpGFk',
     about_role:
       "I'm working remotely. I'm Junior Frontend Developer in eeBrain Team.",
-    cName: "card p-4",
-    begin_date: "February 2021",
-    end_date: "Current",
+    cName: 'card p-4',
+    begin_date: 'February 2021',
+    end_date: 'Current',
   },
   {
-    title: "JamLab",
-    role: "Web Development Intern",
+    title: 'JamLab',
+    role: 'Web Development Intern',
     image:
-      "https://media-exp1.licdn.com/dms/image/C4D0BAQFeCWmZw3ujtg/company-logo_200_200/0/1609868067915?e=1622073600&v=beta&t=0-9CLblO4-xGoZ5Akud-bZ8gpxRZDG9uyBMHSQca9aE",
+      'https://media-exp1.licdn.com/dms/image/C4D0BAQFeCWmZw3ujtg/company-logo_200_200/0/1609868067915?e=1635984000&v=beta&t=i19cb01TygiRV9ZQ6xyRgo_HjPLmQUbMbnnQxGx4QkA',
     about_role:
       "I'm working remotely from Turkey. I'm doing web development related tasks and work HTML, CSS, JavaScript. You can see all my tasks in my GitHub repository.",
-    cName: "card p-4",
-    begin_date: "December 2020",
-    end_date: "March 2021",
+    cName: 'card p-4',
+    begin_date: 'December 2020',
+    end_date: 'March 2021',
   },
-  
-  
 ];
